da4dab592ddfb72528947de387ba9d4e041630fb
BrunoCornejo001 <bcornejo@mail.austral.edu.ar>
2025-04-21 00:48:16 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,14 @@
-def leap_year():
-    print("TO DO")
+def leap_yaer(anio):
+    if (anio % 4 == 0 and anio % 100 != 0) or (anio % 400 == 0):
+        return True
+    else:
+        return False
+
+anio = int(input("Ingrese un año: "))
+
+if es_bisiesto(anio):
+    print(f"El año {anio} es bisiesto")
+else:
+    print(f"El año {anio} no es bisiesto")
+
+leap_year()
