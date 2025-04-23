a975e0c2e4553f397914c3e5dbcedd7e38395a05
BrunoCornejo001 <bcornejo@mail.austral.edu.ar>
2025-04-23 09:15:17 -0300

    Update line.py

--- line.py
+++ line.py
@@ -1,14 +1,16 @@
-import math
 def line():
-    a = float(input("Ingrese el coeficiente A: "))
-    b = float(input("Ingrese el coeficiente B: "))
-    x1 = float(input("Ingrese el coeficiente X1: "))
-    x2 = float(input("Ingrese el coeficiente X2: "))
+    import math
+    
+    a = float(input("Ingrese el coeficiente A:"))
+    b = float(input("Ingrese el coeficiente B:"))
+    x1 = float(input("Ingrese el coeficiente X1:"))
+    x2 = float(input("Ingrese el coeficiente X2:"))
+    
     print(f"El coeficiente A de su ecuación de la recta es: {a}")
     print(f"El coeficiente B de su ecuación de la recta es: {b}")
     print(f"El coeficiente X1 de su ecuación de la recta es: {x1}")
     print(f"El coeficiente X2 de su ecuación de la recta es: {x2}")
-    print("")
+    print()
     print("Para la siguiente ecuación:")
     print(f"\tY = {a}X + {b}")
     print("")
@@ -29,4 +31,3 @@ def line():
     p2 = [x2,y2]
     distancia = math.dist(p2,p1) 
     print(f"La distancia entre ellos es: {distancia}")
-line()
